ce044534085a99fa99e8ee9ccfca658dac179945
mcbeaniee <150880707+mcbeaniee@users.noreply.github.com>
2025-02-25 01:07:59 -0500

    worked on task 5 project

--- topics/exercises/CART263-TASK-5-ALL-STUFF/js/VideoObj.js
+++ topics/exercises/CART263-TASK-5-ALL-STUFF/js/VideoObj.js
@@ -12,8 +12,17 @@ class VideoObj {
  
 
     let filterButton_blur = document.getElementById("filter_button_blur");
+    let filterButton_sepia = document.getElementById("filter_button_sepia");
+    let filterButton_hue = document.getElementById("filter_button_hue");
+    let filterButton_invert = document.getElementById("filter_button_invert");
     let blurInput = document.getElementById("blurnum");
+    let sepiaInput = document.getElementById("sepianum");
+    let hueInput = document.getElementById("huenum");
+    let invertInput = document.getElementById("invertnum");
     this.userProvidedBlur  = 0;
+    this.userProvidedSepia = 0;
+    this.userProvidedHue = 0;
+    this.userProvidedInvert = 0;
     let self = this;
 
     filterButton_blur.addEventListener("click", function () {
@@ -21,11 +30,29 @@ class VideoObj {
       self.userProvidedBlur = blurInput.value;
       console.log(self.userProvidedBlur);
     });
+
+    filterButton_sepia.addEventListener("click", function () {
+      //get value from input field
+      self.userProvidedSepia = sepiaInput.value;
+      console.log(self.userProvidedSepia);
+    });
+
+    filterButton_hue.addEventListener("click", function () {
+      //get value from input field
+      self.userProvidedHue = hueInput.value;
+      console.log(self.userProvidedHue);
+    });
+
+    filterButton_invert.addEventListener("click", function () {
+      //get value from input field
+      self.userProvidedInvert = invertInput.value;
+      console.log(self.userProvidedInvert);
+    });
   }
 
   display() {
     this.context.save();
-     //this.context.filter = `blur(${this.userProvidedBlur}px)`;
+    this.context.filter = `blur(${this.userProvidedBlur}px)`;
     this.context.drawImage(this.videoElement, this.x, this.y, this.w, this.h);
     this.context.fillStyle = this.shapeCol;
     this.context.fillRect(this.shapeX, this.shapeY, 50,50)
